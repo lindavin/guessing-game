9a8e068da18a7be087e6292b1d82ec73b582064f
Davin Lin <lindavin@grinnell.edu>
2020-10-30 19:54:34 -0400

    Set up guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // Game Title
+    println!("Guess the number!");
+    // Ask user for guess.
+    println!("Please input your guess.");
+
+    // Create storage for user input
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
