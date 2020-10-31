ebce17ce3b932e0c9938672829053be185fb8db0
Davin Lin <lindavin@grinnell.edu>
2020-10-31 14:44:51 -0400

    Format with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate rand;
 
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     // Game Title
@@ -12,26 +12,29 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     // Show secret number for internal check
     println!("This is the secret number {}.", secret_number);
-    loop { 
+    loop {
         // Ask user for guess.
         println!("Please input your guess.");
 
         // Create storage for user input
         let mut guess = String::new();
 
-        io::stdin().read_line(&mut guess)
+        io::stdin()
+            .read_line(&mut guess)
             .expect("Failed to read line");
-        
+
         // Convert String to Number
-        let guess: u32 = guess.trim().parse()
-            .expect("Enter a number!");
+        let guess: u32 = guess.trim().parse().expect("Enter a number!");
 
         println!("You guessed: {}", guess);
 
-        match guess.cmp(&secret_number){
+        match guess.cmp(&secret_number) {
             Ordering::Less => println!("Guess is too low!"),
             Ordering::Greater => println!("Guess to is large!"),
-            Ordering::Equal => { println!("You got it!"); break }
+            Ordering::Equal => {
+                println!("You got it!");
+                break;
+            }
         }
     }
 }
