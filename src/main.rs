2fbe95e7274a6f030f29f66430670f9276ae13e9
Davin Lin <lindavin@grinnell.edu>
2020-10-31 14:44:11 -0400

    Add loop

--- src/main.rs
+++ src/main.rs
@@ -12,24 +12,26 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     // Show secret number for internal check
     println!("This is the secret number {}.", secret_number);
-    // Ask user for guess.
-    println!("Please input your guess.");
+    loop { 
+        // Ask user for guess.
+        println!("Please input your guess.");
 
-    // Create storage for user input
-    let mut guess = String::new();
+        // Create storage for user input
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    // Convert String to Number
-    let guess: u32 = guess.trim().parse()
-        .expect("Enter a number!");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        // Convert String to Number
+        let guess: u32 = guess.trim().parse()
+            .expect("Enter a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Guess is too low!"),
-        Ordering::Greater => println!("Guess to is large!"),
-        Ordering::Equal => println!("You got it!")
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Guess is too low!"),
+            Ordering::Greater => println!("Guess to is large!"),
+            Ordering::Equal => { println!("You got it!"); break }
+        }
     }
 }
